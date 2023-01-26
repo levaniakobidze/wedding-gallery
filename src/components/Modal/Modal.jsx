b45c517d9228e870c41a3b9ad4797efb22d62bd7
levaniakobidze <levaniakobidze25@gmail.com>
2023-01-26 23:39:41 +0400

    remove left images from modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import './Modal.css'
-import {IoMdClose} from 'react-icons/io'
+import React, { useEffect, useState } from "react";
+import "./Modal.css";
+import { IoMdClose } from "react-icons/io";
 
-function Modal({photos,modalImgIndex,setModalImgIndex,setShowModal}) {
+function Modal({ photos, modalImgIndex, setModalImgIndex, setShowModal }) {
+  const [url, setUrl] = useState("");
 
-    const [url,setUrl] = useState('')
-
-   useEffect(() => {
-        const img = photos.find((p,i) => i === modalImgIndex) 
-        setUrl(img.url)
-   },[modalImgIndex])
+  useEffect(() => {
+    const img = photos.find((p, i) => i === modalImgIndex);
+    setUrl(img.url);
+  }, [modalImgIndex]);
 
   return (
-    <div className='modal-backdrop'>
-        <div className='close-modal' onClick={() => setShowModal(false)}>
-            <IoMdClose className='close-modal-icon' />
+    <div className="modal-backdrop">
+      <div className="close-modal" onClick={() => setShowModal(false)}>
+        <IoMdClose className="close-modal-icon" />
+      </div>
+      <div className="images">
+        <div className="modal-main-img">
+          <img src={url} alt="image-url" />
         </div>
-        <div className="images">
-         <div className="small-images-cont">
-            {photos.map((p,i) => {
-                return <div className='small-image' onClick={() => setModalImgIndex(i)}>
-                    <img src={p.url} alt="small-image" />
-                </div>
-            })}
-         </div>
-        <div className='modal-main-img'>
-             <img src={url} alt="image-url" />    
-         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
